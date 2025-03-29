63cf8eb4ee167742b3ad0766b64df0e71d6238c2
syed-spikra <syed@spikra.com>
2025-03-29 23:30:00 +0530

    console changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var outputDatares,dataToSend;
 const dateInput = document.getElementById('myDate');
 dateInput.addEventListener('change', function() {
   const selectedDate = dateInput.value; // Returns date in YYYY-MM-DD format
-  // console.log('Selected date:', selectedDate);
+  console.log('Selected date:', selectedDate);
 });
 
 // dateinput = document.getElementById('myDate');
@@ -39,13 +39,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
           if (direction === -1) {
             newValue = (currentValue - stepValue).toFixed(2);
-            console.log(newValue);
+            // console.log(newValue);
             if (newValue >= minValue) {
               numberInput.value = newValue;
             }
           } else if (direction === 1) {
             newValue = (currentValue + stepValue).toFixed(2);
-            console.log(newValue);
+            // console.log(newValue);
             if (newValue <= maxValue) {
               numberInput.value = newValue;
             }
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const newRow = document.createElement('tr');
       newRow.id = "memb"+rcount;
       newRow.innerHTML = '<td> <div class="membername"> <input type="text" placeholder="john Deo"> </div> </td> <td> <div class="role"> <input type="text" placeholder="Team Leader"> </div> </td> <td> <div class="departments"> <select id="departments" name="department"> <option value="Engineer">Engineer</option> <option value="Design">Design</option> <option value="Product">Product</option> <option value="Marketing">Marketing</option> <option value="Others">Others</option> </select> </div> </td> <!-- hrs/day --> <td> <div class="number-input-group"> <input type="number" class="number-input" id="input1" name="input1" value="4.00" step="0.5" min="2" max="100"> <div class="input-controls"> <button type="button" class="minus-button">-</button> <button type="button" class="plus-button">+</button> </div> </div> </td> <!-- $cost/hr --> <td> <div class="number-input-group"> <input type="number" class="number-input" id="input1" name="input1" value="40.00" step="0.5" min="0" max="100"> <div class="input-controls"> <button type="button" class="minus-button">-</button> <button type="button" class="plus-button">+</button> </div> </div> </td> <!-- billable rate $/hr --> <td> <div class="number-input-group"> <input type="number" class="number-input" id="input1" name="input1" value="40.00" step="0.5" min="0" max="100"> <div class="input-controls"> <button type="button" class="minus-button">-</button> <button type="button" class="plus-button">+</button> </div> </div> </td> <!-- billable ratio % --> <td> <div class="number-input-group"> <input type="number" class="number-input" id="input1" name="input1" value="100.00" step="0.5" min="0" max="100"> <div class="input-controls"> <button type="button" class="minus-button">-</button> <button type="button" class="plus-button">+</button> </div> </div> </td>'
-        // console.log(newRow, rcount);
+        console.log(newRow, rcount);
       return newRow;
     }
   
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const currentRowCount = teamMemberTableBody.querySelectorAll('tr').length;
     
         if (desiredMemberCount > currentRowCount) {
-            // console.log("greater value")
+            console.log("greater value")
             // Add rows
             for (let i = 0; i < desiredMemberCount - currentRowCount; i++) {
             teamMemberTableBody.appendChild(createTeamMemberRow(i+2));
@@ -124,8 +124,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // change occurs
     inputContainer.addEventListener('input', handleInputChangeAndApiCall);
     // inputContainer.addEventListener('input', (e)=>{
-        console.log("input change occurs");
-        console.log(e.data);
+    //     console.log("input change occurs");
+    //     console.log(e.data);
     // });
     // ==============================================
 
@@ -133,8 +133,8 @@ document.addEventListener('DOMContentLoaded', () => {
     inputContainer.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
         handleInputChangeAndApiCall();
-        console.log("Enter event occur");
-        console.log(event);
+        // console.log("Enter event occur");
+        // console.log(event);
       }
     });
     // ==============================================
@@ -143,8 +143,8 @@ document.addEventListener('DOMContentLoaded', () => {
     inputContainer.addEventListener('click', function(event) {
       if (event.target.classList.contains('minus-button') || event.target.classList.contains('plus-button')) {
         // small delay to ensure the input value has updated
-        console.log("plus minus occurs");
-        console.log(event.data);
+        // console.log("plus minus occurs");
+        // console.log(event.data);
         setTimeout(handleInputChangeAndApiCall, 100);
         
       }
@@ -181,7 +181,7 @@ function handleInputChangeAndApiCall() {
   
     if (missingFields.length > 0) {
       const missingFieldNames = missingFields.map(field => field.name).join(', ');
-      // console.log(`Field missing: ${missingFieldNames}`);
+      console.log(`Field missing: ${missingFieldNames}`);
       return; // Stop the function if fields are missing
     }
   
@@ -218,12 +218,12 @@ function handleInputChangeAndApiCall() {
           billable_ratio: parseFloat(billableRatioInput.value),
         });
       }else {
-        // console.log(`Missing data in row with ID: ${rowId}`);
+        console.log(`Missing data in row with ID: ${rowId}`);
         hasMissingRowData = true;
       }
     });
     if (hasMissingRowData) {
-        // console.log("Stopping API call due to missing data in team member rows.");
+        console.log("Stopping API call due to missing data in team member rows.");
         return; // Stop the function
       }
 
@@ -242,7 +242,7 @@ function handleInputChangeAndApiCall() {
       // Add other relevant data from your form if needed
     };
   
-    // console.log("data2send||=======|||=====/n", dataToSend);
+    console.log("data2send||=======|||=====/n", dataToSend);
   
     // Make the API call to your Node.js backend
     fetch('https://projection-calc-function.onrender.com/process-project-data', { 
@@ -259,7 +259,7 @@ function handleInputChangeAndApiCall() {
       return response.json(); 
     })
     .then(data => {
-      // console.log('API Response:', data);
+      console.log('API Response:', data);
       outputDatares = data;
       populateDashboard(data);
     })
@@ -333,7 +333,7 @@ function populateDashboard(data) {
         </tr>
       `;
     });
-    console.log("********",costBreakdownHTML);
+    // console.log("********",costBreakdownHTML);
     teamCostBreakdownTableBody.innerHTML = costBreakdownHTML;
     totalCostBreakdownDisplay.textContent = `$ ${(processData.teamCostBreakdown.totalCosts).toFixed(2)}`;
 
@@ -464,7 +464,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     projectOutput: outputData.processData
                 }
             };
-            // console.log("*****",userData);
+            console.log("*****",userData);
             // Call the function to send data to the backend
             sendUserDataToBackend(userData);
 
@@ -487,7 +487,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (response.ok) {
                 const result = await response.json();
-                // console.log('Success:', result);
+                console.log('Success:', result);
                 // Handle success (e.g., show a success message)
             } else {
                 const error = await response.json();
